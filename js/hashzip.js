91bcf4787dfff895b050df0eaf600ce345909d58
Sakis <sakis.stefanidis@student.kdg.be>
2022-05-24 20:08:46 +0200

    Download hash content styling

--- js/hashzip.js
+++ js/hashzip.js
@@ -1,4 +1,4 @@
 export const hashzip = {
-        "sha256" : "[0a7a9f1d87d47589cd5a029ce08f99cd31d3ce2bb4d77ba44316a66c16f72189  /home/202122t14-ssh/../../web/gameUpload/qwirkle.zip]",
-        "sha512" : "[5f28852939e8739d31b098fe5b85a61b5853785e96ac86e3d993dd7d3fd6632b713ace6a0c17e7340d05980b398657b9f3cc9c53ed32eed6ef2251e450792e28  /home/202122t14-ssh/../../web/gameUpload/qwirkle.zip]",
-        "md5" : "[1593cedb6227b3845003d1795c82d1e0  /home/202122t14-ssh/../../web/gameUpload/qwirkle.zip]" }
+        "sha256" : "0a7a9f1d87d47589cd5a029ce08f99cd31d3ce2bb4d77ba44316a66c16f72189  /home/202122t14-ssh/../../web/gameUpload/qwirkle.zip",
+        "sha512" : "5f28852939e8739d31b098fe5b85a61b5853785e96ac86e3d993dd7d3fd6632b713ace6a0c17e7340d05980b398657b9f3cc9c53ed32eed6ef2251e450792e28  /home/202122t14-ssh/../../web/gameUpload/qwirkle.zip",
+        "md5" : "1593cedb6227b3845003d1795c82d1e0  /home/202122t14-ssh/../../web/gameUpload/qwirkle.zip" }
