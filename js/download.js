1c1cb0a482de1cb40a59890c632cff9801dc23fa
seifeldin.sabry <ismailseifeldin54@gmail.com>
2022-05-24 19:48:44 +0200

    pushed a change, modified the javascript download and files that contain hash

--- js/download.js
+++ js/download.js
@@ -1,6 +1,7 @@
 "use strict";
 
-import {createRequire} from "node:module";
+import {hashjar} from "./hashjar.js";
+import {hashzip} from "./hashzip.js";
 
 const hash256jarEl = document.getElementById('sha256jar');
 const hash512jarEl = document.getElementById('sha512jar');
@@ -10,11 +11,18 @@ const hash256zipEl = document.getElementById('sha256zip');
 const hash512zipEl = document.getElementById('sha512zip');
 const hashMdn5zipEl = document.getElementById('md5zip');
 
-const require = createRequire(import.meta.url);
-const hashJar = require("./hashjar.json")
-const hashZip = require("./hashjar.json")
-let {sha256 : sha256Jar,sha512 : sha512Jar,md5 : md5Jar} = hashJar
-let {sha256 : sha256Zip,sha512 : sha512Zip,md5 : md5Zip} = hashZip
+
+let {sha256 : sha256Jar,sha512 : sha512Jar,md5 : md5Jar} = hashjar
+let {sha256 : sha256Zip,sha512 : sha512Zip,md5 : md5Zip} = hashzip
+
+sha256Jar = sha256Jar.split(" ")[0];
+sha512Jar = sha512Jar.split(" ")[0];
+md5Jar = md5Jar.split(" ")[0];
+
+sha256Zip = sha256Zip.split(" ")[0];
+sha512Zip = sha512Zip.split(" ")[0];
+md5Zip = md5Zip.split(" ")[0];
+
 
 hash256jarEl.textContent = sha256Jar;
 hash512jarEl.textContent = sha512Jar;
