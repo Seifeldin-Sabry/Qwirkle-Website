90d658a045a1edcfe88a76f516d79092836f576f
qwirklehelp <serverUserQwirklet14@outlook.com>
2022-05-24 17:34:38 +0200

    added json

---
+++ js/hashjar.js
@@ -0,0 +1,4 @@
+export const hashjar = [ {
+        "sha256" = "[398d628ce91416357ad32c429665d824fa0d02619eb153f3dce8163391d51a1f  /home/202122t14-ssh/../../web/gameUpload/qwirkle.jar]"
+        "sha512" = "[9a23fad489f935c676ad7c182530a4d44a7a2354aeb4167d685d17f0ec62f8932134acc7f9fc154dc1edf5f919743a4f9bf635a48f864a3814573a3190610c08  /home/202122t14-ssh/../../web/gameUpload/qwirkle.jar]"
+        "md5" = "[3ca7101a2e95aa22987853e6a7e9cfcc  /home/202122t14-ssh/../../web/gameUpload/qwirkle.jar]" 
