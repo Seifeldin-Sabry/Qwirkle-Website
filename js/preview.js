50d3d408f1ccd9c65c997885eb3f778d0fdb366f
Sakis <sakis.stefanidis@student.kdg.be>
2022-05-25 14:32:17 +0200

    Preview js done

---
+++ js/preview.js
@@ -0,0 +1,145 @@
+const body = document.getElementById("body");
+const footer = document.getElementById("footer");
+const preview1 = document.getElementById("img_preview1");
+const target1 = document.getElementById("img1");
+target1.addEventListener("click", openPreview1);
+preview1.addEventListener("click", openPreview1);
+function openPreview1() {
+    if (preview1.style.display === "block") {
+        preview1.style.display = "none";
+        body.style.overflow = "visible";
+        footer.style.display = "block";
+    } else {
+        preview1.style.display = "block";
+        body.style.overflow = "hidden";
+        footer.style.display = "none";
+    }
+};
+
+const preview2 = document.getElementById("img_preview2");
+const target2 = document.getElementById("img2");
+target2.addEventListener("click", openPreview2);
+preview2.addEventListener("click", openPreview2);
+function openPreview2() {
+    if (preview2.style.display === "block") {
+        preview2.style.display = "none";
+        body.style.overflow = "visible";
+        footer.style.display = "block";
+    } else {
+        preview2.style.display = "block";
+        body.style.overflow = "hidden";
+        footer.style.display = "none";
+    }
+};
+
+const preview3 = document.getElementById("img_preview3");
+const target3 = document.getElementById("img3");
+target3.addEventListener("click", openPreview3);
+preview3.addEventListener("click", openPreview3);
+function openPreview3() {
+    if (preview3.style.display === "block") {
+        preview3.style.display = "none";
+        body.style.overflow = "visible";
+        footer.style.display = "block";
+    } else {
+        preview3.style.display = "block";
+        body.style.overflow = "hidden";
+        footer.style.display = "none";
+    }
+};
+
+const preview4 = document.getElementById("img_preview4");
+const target4 = document.getElementById("img4");
+target4.addEventListener("click", openPreview4);
+preview4.addEventListener("click", openPreview4);
+function openPreview4() {
+    if (preview4.style.display === "block") {
+        preview4.style.display = "none";
+        body.style.overflow = "visible";
+        footer.style.display = "block";
+    } else {
+        preview4.style.display = "block";
+        body.style.overflow = "hidden";
+        footer.style.display = "none";
+    }
+};
+
+const preview5 = document.getElementById("img_preview5");
+const target5 = document.getElementById("img5");
+target5.addEventListener("click", openPreview5);
+preview5.addEventListener("click", openPreview5);
+function openPreview5() {
+    if (preview5.style.display === "block") {
+        preview5.style.display = "none";
+        body.style.overflow = "visible";
+        footer.style.display = "block";
+    } else {
+        preview5.style.display = "block";
+        body.style.overflow = "hidden";
+        footer.style.display = "none";
+    }
+};
+
+const preview6 = document.getElementById("img_preview6");
+const target6 = document.getElementById("img6");
+target6.addEventListener("click", openPreview6);
+preview6.addEventListener("click", openPreview6);
+function openPreview6() {
+    if (preview6.style.display === "block") {
+        preview6.style.display = "none";
+        body.style.overflow = "visible";
+        footer.style.display = "block";
+    } else {
+        preview6.style.display = "block";
+        body.style.overflow = "hidden";
+        footer.style.display = "none";
+    }
+};
+
+const preview7 = document.getElementById("img_preview7");
+const target7 = document.getElementById("img7");
+target7.addEventListener("click", openPreview7);
+preview7.addEventListener("click", openPreview7);
+function openPreview7() {
+    if (preview7.style.display === "block") {
+        preview7.style.display = "none";
+        body.style.overflow = "visible";
+        footer.style.display = "block";
+    } else {
+        preview7.style.display = "block";
+        body.style.overflow = "hidden";
+        footer.style.display = "none";
+    }
+};
+
+const preview8 = document.getElementById("img_preview8");
+const target8 = document.getElementById("img8");
+target8.addEventListener("click", openPreview8);
+preview8.addEventListener("click", openPreview8);
+function openPreview8() {
+    if (preview8.style.display === "block") {
+        preview8.style.display = "none";
+        body.style.overflow = "visible";
+        footer.style.display = "block";
+    } else {
+        preview8.style.display = "block";
+        body.style.overflow = "hidden";
+        footer.style.display = "none";
+    }
+};
+
+const preview9 = document.getElementById("img_preview9");
+const target9 = document.getElementById("img9");
+target9.addEventListener("click", openPreview9);
+preview9.addEventListener("click", openPreview9);
+function openPreview9() {
+    if (preview9.style.display === "block") {
+        preview9.style.display = "none";
+        body.style.overflow = "visible";
+        footer.style.display = "block";
+    } else {
+        preview9.style.display = "block";
+        body.style.overflow = "hidden";
+        footer.style.display = "none";
+    }
+};
