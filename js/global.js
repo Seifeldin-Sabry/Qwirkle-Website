0030f854a85681754af4461f90e798b8081d1b63
Sakis <sakis.stefanidis@student.kdg.be>
2022-05-27 03:50:36 +0200

    Added reload function to logout action and gitlab icon to Download page

--- js/global.js
+++ js/global.js
@@ -2,6 +2,7 @@
 
 const h2logoutEl = document.querySelector('#h2-logout');
 const logoutEl = document.querySelector('#logout');
+
 const isLoggedIn = () => {
     return JSON.parse(localStorage.getItem("isLoggedIn"))
 }
@@ -10,11 +11,13 @@ logoutEl.addEventListener('click', logout);
 function logout() {
     h2logoutEl.classList.add('hidden');
     localStorage.clear();
+    location.reload();
 }
 function showLogout() {
     if (localStorage.getItem('isLoggedIn') != undefined) {
         h2logoutEl.classList.remove('hidden');
     }
 }
+
 window.onload = showLogout;
 
