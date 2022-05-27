95bb1ef6eb0f2d8d2cd2fd1d4c0a18aae72074d8
Sakis <sakis.stefanidis@student.kdg.be>
2022-05-27 15:48:28 +0200

    fixed a typo

--- js/global.js
+++ js/global.js
@@ -12,8 +12,9 @@ function logout() {
     h2logoutEl.classList.add('hidden');
     localStorage.clear();
     location.reload();
-    if (window.location.pathname ==="statistics-logged.html"){}
-    window.location.pathname = 'statistics.html';
+    if (window.location.pathname ==="statistics-logged.html") {
+        window.location.pathname = 'statistics.html';
+    }
 }
 function showLogout() {
     if (JSON.parse(localStorage.getItem('isLoggedIn')) === true) {
